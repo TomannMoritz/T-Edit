e9e910de30a3da2a5ff130d938c80a73f8a98b9d
TomannMoritz <164042654+TomannMoritz@users.noreply.github.com>
2025-08-17 09:38:58 +0200

    build/test: build command: add test step & test included files

--- build.zig
+++ build.zig
@@ -1,5 +1,8 @@
 const std = @import("std");
 pub fn build(b: *std.Build) void {
+    // --------------------------------------------------
+    // Create executable
+    // --------------------------------------------------
     const exe = b.addExecutable(.{
         .name = "Tedit",
         .root_module = b.createModule(.{
@@ -10,9 +13,24 @@ pub fn build(b: *std.Build) void {
     });
 
     b.installArtifact(exe);
-
-    const run_exe = b.addRunArtifact(exe);
+    const add_exe = b.addRunArtifact(exe);
 
     const run_step = b.step("run", "Run the application");
-    run_step.dependOn(&run_exe.step);
+    run_step.dependOn(&add_exe.step);
+
+
+    // --------------------------------------------------
+    // Tests
+    // --------------------------------------------------
+    const unit_tests = b.addTest(.{
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = b.graph.host,
+        })
+    });
+
+    const add_test = b.addRunArtifact(unit_tests);
+
+    const run_test = b.step("test", "Run unit tests");
+    run_test.dependOn(&add_test.step);
 }
--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,28 @@
 
-
+// --------------------------------------------------
+// imports
 const std = @import("std");
 
+
+// --------------------------------------------------
+// local imports
+const gap_buffer = @import("gap_buffer.zig");
+
+
+// --------------------------------------------------
 pub fn main() !void {
-    std.debug.print("Main\n", .{});
+    std.debug.print("Main:\n", .{});
+
 }
+
+
+
+// --------------------------------------------------
+// Testing
+
+test {
+    // Test imports
+     std.testing.refAllDecls(@This());
+}
+
+
